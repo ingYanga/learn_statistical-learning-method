93e72e42d99fd53c14e45f76fb37dc70a9d3d23e
tenshine <tanwnx@outlook.com>
2019-03-20 00:07:30 +0800

    第4章 朴素贝叶斯(NaiveBayes)

---
+++ code/第4章 朴素贝叶斯(NaiveBayes)/README.md
@@ -0,0 +1,49 @@
+# 第4章 朴素贝叶斯
+
+## 介绍
+
++ 方便阅读
+`naive.ipynb`
+
++ 方便复制粘贴
+`naive.py`
+
+## 完成这一章所写的博客
+
++ [最大似然估计](https://www.jianshu.com/p/af06571326ff)
+
++ [最大似然估计-高斯分布](https://www.jianshu.com/p/0c918fdea051)
+
++ [实现朴素贝叶斯分类器](https://www.jianshu.com/p/01efe1a39efb)
+
+## 完成这一章所看的资料
+
+### 博客资料
+
++ [详解最大似然估计（MLE）、最大后验概率估计（MAP），以及贝叶斯公式的理解](https://blog.csdn.net/u011508640/article/details/72815981)
+
++ [一文搞懂极大似然估计](https://zhuanlan.zhihu.com/p/26614750)
+
++ [贝叶斯估计、最大似然估计、最大后验概率估计](http://noahsnail.com/2018/05/17/2018-05-17-%E8%B4%9D%E5%8F%B6%E6%96%AF%E4%BC%B0%E8%AE%A1%E3%80%81%E6%9C%80%E5%A4%A7%E4%BC%BC%E7%84%B6%E4%BC%B0%E8%AE%A1%E3%80%81%E6%9C%80%E5%A4%A7%E5%90%8E%E9%AA%8C%E6%A6%82%E7%8E%87%E4%BC%B0%E8%AE%A1/)
+
+### 视频资料
+
++ [13_最大似然估计【小元老师】考研数学，概率论与数理统计](https://www.bilibili.com/video/av15944258?from=search&seid=10919302286524765842)
+
++ [机器学习课程 Expectation Maximisation (part 1)](https://www.youtube.com/watch?v=Bq5s80ZCmC0&t=290s)
+
++ [机器学习课程 Expectation Maximisation (part 2)](https://www.youtube.com/watch?v=C6xjPAIeeRk)
+
++ [机器学习课程 Expectation Maximisation (part 3)](https://www.youtube.com/watch?v=rSC4mGavgTs)
+
++ [机器学习课程 Expectation Maximisation (part 4)](https://www.youtube.com/watch?v=Vv-rEpGH-kE)
+
++ [机器学习课程 Expectation Maximisation (part 5)](https://www.youtube.com/watch?v=7KgT0WFk4AU)
+
++ [机器学习课程 Expectation Maximisation (part 6)](https://www.youtube.com/watch?v=mYNmWKTsOxo)
+
++ [机器学习课程 Expectation Maximisation (part 7)](https://www.youtube.com/watch?v=2wnnKyDTJdc)
+
+### 代码参考
+
++ [GaussianNB](https://github.com/wzyonggege/statistical-learning-method/blob/master/NaiveBayes/GaussianNB.ipynb)
--- code/第4章 朴素贝叶斯(NaiveBayes)/naive.py
+++ code/第4章 朴素贝叶斯(NaiveBayes)/naive.py
@@ -6,10 +6,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from DrawTools import DrawTools
-
-
-# data
+# 创建数据
 def create_data():
     iris = load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -17,10 +14,9 @@ def create_data():
     df.columns = ['sepal length', 'sepal width',
                   'petal length', 'petal width', 'label']
     data = np.array(df.iloc[:100, :])
-    # print(data)
     return data[:, :-1], data[:, -1]
 
-
+# 利用高斯分布估计概率
 class NaiveBayes:
     def __init__(self):
         self.model = None
@@ -35,22 +31,28 @@ class NaiveBayes:
         avg = self.mean(X)
         return math.sqrt(sum([pow(x-avg, 2) for x in X]) / float(len(X)))
 
-    # 概率密度函数
+    # 概率密度函数 用概率密度代替概率
     def gaussian_probability(self, x, mean, stdev):
         exponent = math.exp(-(math.pow(x-mean, 2)/(2*math.pow(stdev, 2))))
         return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
-++
+    
     def summarize(self, train):
+        """
+        :param train [[1, 2, 3], [4, 5, 6]]
+        """
+        # *train 可以得到所有元素，按照上面的例子 print(*train)
+        # 得到 [1, 2, 3] [4, 5, 6]
+        # 所以 zip(*train) 
+        # 得到 [1, 4]
+        # 所以 i 就是每一个特征的集合
+        # 然后计算平均和标准差
         s = [(self.mean(i), self.stdev(i)) for i in zip(*train)]
         return s
 
     def fit(self, X, y):
         """
-        这个函数用来得到概率密度函数里面的标准差和平均数
-        而且每个标签都要分开来算
-
-        : para X 数组 [[1, 2, 3], [1, 2, 3]]
-        : para y 是标签 [0, 1]
+        :param X 数组 [[1, 2, 3], [1, 2, 3]]
+        :param y 是标签 [0, 1]
         """
         labels = list(set(y))
         data = {label: [] for label in labels}
@@ -61,48 +63,61 @@ class NaiveBayes:
         # 分类后，计算每个类中的每个特征的平均数和标准差
         self.model = {label: self.summarize(value)
                       for label, value in data.items()}
-
-        return '训练完毕'
+        
+        # 计算 py。并把 py 放在数组最后面
+        for label, value in data.items():
+            self.model[label].append(len(value)/len(X))
+        
+        print('train done!')
 
     def cal_probabilities(self, input_data):
         """
-        计算每个分类的可能性
+        计算每个类别的可能性
         """
         probabilities = {}
         for label, value in self.model.items():
             probabilities[label] = 1
+            # 贝叶斯公式右边一项 p(y)
+            py = value[-1]
             for i in range(len(input_data)):
                 _mean, _stdev = value[i]
-                # 贝叶斯公式分子的左部分
+                # 贝叶斯公式 p(y|x)
                 probabilities[label] *= self.gaussian_probability(
                     input_data[i], _mean, _stdev)
+            probabilities[label] *= py
         return probabilities
 
     def predict(self, input_data):
+        """
+        cal_probabilities(input_data)
+        返回一个字典
+        items()
+        返回一个 tupple 
+        用 tupple 最后一项排序
+        得到排序后的序列
+        选最后一个概率最大的
+        返回 label
+        """
         label = sorted(self.cal_probabilities(
             input_data).items(), key=lambda x: x[-1])[-1][0]
         return label
-
+    
+    def score(self, X, y):
+        r = 0.0
+        for x, real_y in zip(X, y):
+            predict_y = self.predict(x)
+            if predict_y == real_y:
+                r += 1
+                
+        return r/len(y)
 
 def main():
     X, y = create_data()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    # print('X, y', X, y)
-    # print('X_train, X_test, y_train, y_test', X_train, X_test, y_train, y_test)
     model = NaiveBayes()
-    print(model.fit(X_train, y_train))
+    model.fit(X_train, y_train)
 
-    # print(model.model.items())
-    # model = model.model
-    # mean, stdev = model[0][1]
-    mean, stdev = (0.5, 1)
-
-    def func(x):
-        exponent = math.exp(-(math.pow(x-mean, 2)/(2*math.pow(stdev, 2))))
-        return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
-
-    dt = DrawTools()
-    dt.draw_line_with_func(func=func, step=0.01, x_range=(0, 1))
+    print(model.score(X_test, y_test))
 
 
 if __name__ == "__main__":
